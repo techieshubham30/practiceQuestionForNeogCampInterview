f90bfbb9eb4dfb35788529ffffc4bcd28139d6b8
techieshubham30 <82032825+techieshubham30@users.noreply.github.com>
2021-09-06 14:22:56 +0530

    initial commit

--- samplequestion11.js
+++ samplequestion11.js
@@ -7,21 +7,33 @@ const submitButton=document.querySelector('.submit-button');
 
 submitButton.disabled=true;
 
+inputPassword.addEventListener("keyup",unableDisable);
+
 checkPassword.addEventListener("click",validatePassword);
 showPassword.addEventListener("click",showPass);
 
 hidePassword.addEventListener("click",hidePass);
 
+
+function unableDisable(){
+    var passwordLength=inputPassword.value.length;
+    console.log(passwordLength);
+    if(passwordLength>=10){
+        submitButton.disabled=false;
+    }else{
+        submitButton.disabled=true;
+    }
+}
 function validatePassword(){
     var passwordLength=inputPassword.value.length;
     console.log(passwordLength);
 
     if(passwordLength>=10){
         output.innerText="success";
-        submitButton.disabled=false;
+        // submitButton.disabled=false;
 
     }else{
-        submitButton.disabled=true;
+        // submitButton.disabled=true;
 
         output.innerText="error";
 
